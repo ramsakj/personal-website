0d82046c86d7d815ae9ad44d41973ba8a4a44898
ramsakj <jure2310@gmail.com>
2023-04-14 11:06:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home_page():
-    return render_template('home_page.html')
+def home():
+    return render_template('index.html')
 
 @app.route("/portfolio")
 def portfolio():
