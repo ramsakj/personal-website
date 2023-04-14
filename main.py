cc4d2a1b337bacaae39ee16e764a570f7c479f35
ramsakj <jure2310@gmail.com>
2023-04-14 11:10:36 +0200

    ..

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('home_page.html')
 
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
-
-@app.route("/portfolio/hairdresser")
-def hairdresser():
-    return render_template("hairdresser.html")
 
 if __name__ == "__main__":
     app.run(use_reloader=True )
