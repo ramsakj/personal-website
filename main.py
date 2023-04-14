b0f646d37b0d796a26f130e831abf9fe1e3dceda
ramsakj <jure2310@gmail.com>
2023-04-14 10:43:02 +0200

    ..

--- main.py
+++ main.py
@@ -6,21 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about-me")
-def about():
-    return render_template("about.html")
-
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
-
-@app.route("/portfolio/fakebook")
-def fakebook():
-    return render_template("fakebook.html")
-
-@app.route("/portfolio/boogle")
-def boogle():
-    return render_template("boogle.html")
 
 @app.route("/portfolio/hairdresser")
 def hairdresser():
