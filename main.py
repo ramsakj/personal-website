60c03c8e97a33b9b89c930c7a11399a24183109e
ramsakj <jure2310@gmail.com>
2023-04-14 14:54:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('home_page.html')
 
+@app.route("/registracija", methods = ['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route("/prijava")
+def login():
+    return render_template('login.html')
+
+@app.route("/statenis")
+def statenis():
+    return render_template('statenis.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/o statenis")
+def about():
+    return render_template('about.html')
+
+@app.route("/kontakt", methods = ['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     app.run(use_reloader=True )
